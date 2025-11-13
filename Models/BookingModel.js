71c4f0753ed9a0552f5ff175742176bcbc5cedcc
piyushvj00 <piyush@7unique.in>
2025-11-13 11:40:36 +0530

    booking schema

--- Models/BookingModel.js
+++ Models/BookingModel.js
@@ -2,29 +2,73 @@ const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema(
   {
-    userId: {
+    business_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Business",
+      required: true,
+    },
+
+    table_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Table",
+      required: true,
+    },
+
+    user_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-    vendorId: {
+
+    schedule_id: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+      ref: "Schedule",
     },
-    tableNumber: { type: String },
-    totalAmount: { type: Number, default: 0 },
-    bookingDate: { type: Date, default: Date.now },
+
+    food_items: [
+      {
+        food_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Food",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+
+    drink_items: [
+      {
+        drink_id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Drink",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+
     status: {
       type: String,
-      enum: ["pending", "completed", "cancelled"],
+      enum: ["pending", "confirmed", "completed", "cancelled"],
       default: "pending",
     },
+    totalAmount: { type: Number, default: 0 },
+    
     paymentStatus: {
       type: String,
       enum: ["unpaid", "paid", "refunded"],
       default: "unpaid",
     },
+    
+    bookingDate: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
